57e8503d412cc00cbe53c078acf4272d25e0887a
Ganesh Chaturthi CJ <ganeshchaturthicj@gmail.com>
2023-09-16 09:13:46 +0530

    Added Slider

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,14 @@ import Navbar from "./Navbar/navbar.jsx";
 import DataGrid from "./DataGrid/dataGrid";
 import { slides } from "./Banner/carouselData.json";
 import { Carousel } from "./Banner/carousel";
-
-
-
+import OtherCompanies from "./OtherCompanies/otherCompanies";
 
 function App() {
   return (
     <>
       <Navbar />
       <Carousel data={slides} />
+      <OtherCompanies />
       <DataGrid />
     </>
   );
---
+++ src/OtherCompanies/otherCompanies.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+import "./otherCompanies.css";
+
+const OtherCompanies = () => {
+  return (
+    <div className="otherCompaniesContainer">
+      <div className="otherCompaniesBody">
+        <div class="slider">
+          <div class="slide-track">
+            <div class="slide">
+              <img
+                src="https://i.imgur.com/woCxpkj.jpg"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div class="slide">
+              <img
+                src="https://i.imgur.com/jmj8Sh2.jpg"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div class="slide">
+              <img
+                src="https://i.imgur.com/ngYgFnn.jpg"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div class="slide">
+              <img
+                src="https://i.imgur.com/1s5DUbJ.jpg"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div class="slide">
+              <img
+                src="https://imgur.com/NHsx95l.jpg"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div class="slide">
+              <img
+                src="https://i.imgur.com/MtEgYbY.jpg"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div class="slide">
+              <img
+                src="https://i.imgur.com/MjNWzhO.jpg"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div class="slide">
+              <img
+                src="https://i.imgur.com/5w1ZWre.jpg"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div class="slide">
+              <img
+                src="https://i.imgur.com/ABXtHKa.jpg"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div class="slide">
+              <img
+                src="https://i.imgur.com/X4YQJZf.jpg"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div class="slide">
+              <img
+                src="https://i.imgur.com/hGWWupT.jpg"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div class="slide">
+              <img
+                src="https://photos.marinetraffic.com/ais/showphoto.aspx?photoid=3406596"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div class="slide">
+              <img
+                src="https://i.imgur.com/woCxpkj.jpg"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+            <div class="slide">
+              <img
+                src="https://i.imgur.com/woCxpkj.jpg"
+                height="100"
+                width="250"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OtherCompanies;
